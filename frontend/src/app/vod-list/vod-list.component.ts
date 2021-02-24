454f0dae0d421b0dfc81d2379a4335a8211e2df2
Miha Maček <miha.macek5@gmail.com>
2021-02-24 20:59:09 +0100

    feat: Add placeholder lazy loading cards and change lazy loading

--- frontend/src/app/vod-list/vod-list.component.ts
+++ frontend/src/app/vod-list/vod-list.component.ts
@@ -8,6 +8,10 @@ import { VodsService } from '../vods.service';
 })
 export class VodListComponent implements OnInit {
   public vodList: Array<any>;
+  public isLoading: boolean = false;
+  public isFirstLoad: boolean = true;
+  public loadedAll: boolean = false;
+
   private pageNum: number = 1;
 
   constructor(private vodsService: VodsService) {
@@ -15,16 +19,37 @@ export class VodListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.vodsService.getVods(this.pageNum, 20).subscribe((data) => {
-      this.vodList = this.vodList.concat(data);
+    this.fetchVods();
+    this.onScrollDown();
+  }
+
+  fetchVods(): void {
+    this.isLoading = true;
+    this.vodsService.getVods(this.pageNum, 15).subscribe((data) => {
+      if (data.length) {
+        this.vodList.push(...data);
+      } else {
+        this.loadedAll = true;
+      }
+      this.isLoading = false;
+      this.isFirstLoad = false;
     });
   }
 
-  onScrollDown() {
-    this.pageNum++;
-    this.vodsService.getVods(this.pageNum, 20).subscribe((data) => {
-      this.vodList = this.vodList.concat(data);
-    });
+  onScrollDown(): void {
+    window.onscroll = () => {
+      const isBottom = this.isBottom();
+      if (!isBottom && !this.loadedAll) {
+        this.pageNum++;
+        this.fetchVods();
+      }
+    }
   }
 
+  isBottom(): boolean {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      return false;
+    }
+    return true;
+  }
 }

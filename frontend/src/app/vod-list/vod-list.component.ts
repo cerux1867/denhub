9bb48f4d03e03da06ccd13388ef3e68ca7260bba
Miha Maček <miha.macek5@gmail.com>
2021-03-02 20:35:17 +0100

    feat: Add searching by title on the frontend (#11)
    * feat: Add searching on the frontend
    
    * refactor: Change checking when to fetch vods on scroll

--- frontend/src/app/vod-list/vod-list.component.ts
+++ frontend/src/app/vod-list/vod-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { VodsService } from '../vods.service';
 import { IChannel } from './IChannel';
 
@@ -8,41 +10,31 @@ import { IChannel } from './IChannel';
   styleUrls: ['./vod-list.component.scss']
 })
 export class VodListComponent implements OnInit {
-  public vodList: Array<any>;
   public isLoading: boolean = false;
   public isFirstLoad: boolean = true;
   public loadedAll: boolean = false;
+  public subject = new Subject<any>();
   public availableChannelList = new Array<IChannel>({
     displayName: 'EsfandTV',
     channelName: 'esfandtv'
   });
   public selectedChannel: IChannel = this.availableChannelList[0];
 
-  private pageNum: number = 1;
+  public vodList$: Observable<any>;
+  public subject$ = this.subject.asObservable();
+  public vodList: Array<any>;
+
+  private _pageNum: number = 1;
+  private _titleFilter: string = "";
 
   constructor(private vodsService: VodsService) {
     this.vodList = []
-  }
-
-  ngOnInit(): void {
-    this.fetchVods();
-    this.onScrollDown();
-  }
-
-  onChannelChange(channel: any): void {
-    this.selectedChannel = channel;
-    this.vodList = [];
-    this.isFirstLoad = true;
-    this.loadedAll = false;
-    this.pageNum = 1;
-    
-    this.fetchVods();
-    this.onScrollDown();
-  }
-
-  fetchVods(): void {
-    this.isLoading = true;
-    this.vodsService.getVods(this.selectedChannel.channelName, this.pageNum, 15).subscribe((data) => {
+    this.vodList$ = this.subject$.pipe(
+      switchMap(() => {
+        return this.vodsService.getVods(this.selectedChannel.channelName, this._pageNum, 15, this._titleFilter)
+      })
+    );
+    this.vodList$.subscribe((data) => {
       if (data.length) {
         this.vodList.push(...data);
       } else {
@@ -53,11 +45,39 @@ export class VodListComponent implements OnInit {
     });
   }
 
+  ngOnInit(): void {
+    this.fetchVods();
+    this.onScrollDown();
+  }
+
+  onChannelChange(channel: any): void {
+    this.selectedChannel = channel;
+    this.resetVodList();
+    this.fetchVods();
+  }
+
+  onTitleFilterInputChange(eventTarget: any): void {
+    if (eventTarget.value.length >= 3) {
+      this._titleFilter = eventTarget.value.trim();
+      this.resetVodList();
+      this.fetchVods();
+    } else if (eventTarget.value === "") {
+      this._titleFilter = "";
+      this.resetVodList();
+      this.fetchVods();
+    }
+  }
+
+  fetchVods(): void {
+    this.isLoading = true;
+    this.subject.next();
+  }
+
   onScrollDown(): void {
     window.onscroll = () => {
       const isBottom = this.isBottom();
-      if (!isBottom && !this.loadedAll) {
-        this.pageNum++;
+      if (!isBottom && !this.loadedAll && !this.isLoading) {
+        this._pageNum++;
         this.fetchVods();
       }
     }
@@ -68,5 +88,12 @@ export class VodListComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+  resetVodList(): void {
+    this.vodList = [];
+    this.isFirstLoad = true;
+    this.loadedAll = false;
+    this._pageNum = 1;
   }
 }

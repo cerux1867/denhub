30f0b1268ddc56bd4ddd9cf9a543d5dac96b6c22
Miha Maček <miha.macek5@gmail.com>
2021-03-03 22:02:52 +0100

    chore: Add better configuration options on the frontend (#17)

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  denhubApiUrl: process.env.DENHUB_API_URL || 'https://localhost:5001'
 };
 
 /*

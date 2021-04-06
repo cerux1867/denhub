8969294d30e18ca6fbeb2b09e16d354a3e9dbf55
Miha Maček <miha.macek5@gmail.com>
2021-04-06 18:08:18 +0200

    refactor: Change used queueing system from Redis to RabbitMQ

---
+++ backend/Denhub.Chat.Collector/Models/QueueSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Denhub.Chat.Collector.Models {
+    public class QueueSettings {
+        public string QueueName { get; set; }
+        public string ExchangeName { get; set; }
+        public string ConnectionString { get; set; }
+
+        public QueueSettings() {
+            QueueName = "chat-messages";
+            ExchangeName = "denhub-exchange";
+        }
+    }
+}
